ac8048c87ec493fbdeee2258543ccd41971c5485
allysson <j.allysson@acad.ifma.edu.br>
2025-09-22 21:31:31 -0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: 'https://allysunner.github.io/smart-shark/',
 };
 
 export default nextConfig;
